052fb161e998925733aef47cb6a5fec900155c32
jonnie254 <johnkamanu254@proton.me>
2024-06-07 18:30:37 +0300

    added code

--- notebook-endpoints/backend/src/router/notebook.router.ts
+++ notebook-endpoints/backend/src/router/notebook.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { addNoteBook } from "../controller/notebook.controller";
 import { getALLNoteBooks } from "../controller/notebook.controller";
+import { getOneNote } from "../controller/notebook.controller";
 
 let notebookRouter = Router();
 notebookRouter.post("/addNoteBook", addNoteBook);
 notebookRouter.get("/getNoteBooks", getALLNoteBooks);
+notebookRouter.get("/getNotebook/:id", getOneNote);
 export { notebookRouter };
